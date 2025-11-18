dac0118bd3814e04772ad5a8e377a80cef990f37
carlos-manuel-diaz <43973506+carlos-manuel-diaz@users.noreply.github.com>
2025-11-17 21:25:13 -0500

    implemented simple graphing

--- src/backend.py
+++ src/backend.py
@@ -10,7 +10,7 @@ class TrackerEngine:
         self.csvList = []
         self.df = None
 
-    def getCsv(self):
+    def addCsv(self):
         file_path, _ = QFileDialog.getOpenFileName(
             None,
             "Open CSV File",
@@ -21,12 +21,26 @@ class TrackerEngine:
         if not file_path:
             return
 
+
+        # print(f"Loaded CSV: {file_path}")
+        # print(f"Rows: {len(self.df)} Columns: {list(self.df.columns)}")
+        return file_path
+
+    def extract(self, path):
         try:
-            self.df = pd.read_csv(file_path)
+            self.df = pd.read_csv(path)
         except Exception as e:
             print(f"Failed to read CSV: {e}")
-            return
+            return None, None
+        
+        time_col = "time_elapsed"
+        value_col = "acceleration_y"
 
-        print(f"Loaded CSV: {file_path}")
-        print(f"Rows: {len(self.df)} Columns: {list(self.df.columns)}")
-        return file_path
+        # __ over ___ function
+        if time_col not in self.df.columns or value_col not in self.df.columns:
+            print(f"Required columns not found: need '{time_col}' and '{value_col}'")
+            return
+        
+        return self.df[time_col], self.df[value_col]
+
+    
--- src/frontend.py
+++ src/frontend.py
@@ -1,8 +1,9 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
 from PySide6.QtCore import Qt
-from .ui.orion_v0 import Ui_sheetView
+from .ui.orion_v1 import Ui_sheetView
 from .backend import TrackerEngine
+
 
 class FlightTrackerApp(QMainWindow):
     def __init__(self):
@@ -13,17 +14,44 @@ class FlightTrackerApp(QMainWindow):
         self.engine = TrackerEngine()
         self.connections()
 
+        self.ui.graphWidget.hide()
+
         self.setWindowTitle("Orion")
         self.setFixedSize(800, 600)
 
     def connections(self):
         self.ui.importButton.clicked.connect(self.import_clicked)
+        self.ui.graphView.clicked.connect(self.graph_clicked)
+        # self.ui.pushButton_4.clicked.connect(self.sheet_clicked)
         print('connected')
 
-    def import_clicked(self):
+    def import_clicked(self): #import button clicked
         print('import clicked!')
-        csv_path = self.engine.getCsv()
+        csv_path = self.engine.addCsv()
         self.ui.csvList.addItem(csv_path)
+        self.engine.csvList.append(csv_path)
+
+    def graph_clicked(self): #graph button clicked
+        print('Switching to graph view')
+        self.build_graph(self.ui.csvList.currentText())
+        self.ui.graphWidget.show()
+
+
+    def build_graph(self, path): #build graph with currently selected path
+        current_csv = path
+
+        if not current_csv:
+            print("No CSV selected.")
+            return
+
+        time_x, accel_y = self.engine.extract(current_csv)
+
+        if time_x is None or accel_y is None:
+            print("Extraction failed.")
+            return
+
+        self.ui.graphWidget.clear()    
+        self.ui.graphWidget.plot(time_x, accel_y)
 
 
 
@@ -33,7 +61,9 @@ def main():
     app.setApplicationName("Orion")
     app.setApplicationVersion("1.0")
 
-    window = FlightTrackerApp()   
+
+    window = FlightTrackerApp()
+    window.setWindowIcon(QIcon("assets/seds.png"))
     window.show()
 
     sys.exit(app.exec())
